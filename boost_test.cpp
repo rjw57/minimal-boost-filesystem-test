8e2e999ddef0000b5780ad8304a5672142e9ca3b
Rich Wareham <rjw57@cam.ac.uk>
2012-05-22 10:22:56 +0100

    initial import

---
+++ boost_test.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#include <boost/filesystem.hpp>
+
+int main(int argc, char** argv)
+{
+	namespace bf = boost::filesystem;
+	bf::path hosts_file("/etc/hosts");
+
+	if(!bf::exists(hosts_file)) {
+		std::cout << hosts_file << ": does not exist.\n";
+	} else {
+		std::cout << hosts_file << ": exists.\n";
+	}
+
+	return EXIT_SUCCESS;
+}
